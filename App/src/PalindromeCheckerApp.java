c8f3a7924107fcbf2263b1d5d35cc0d6b8791baf
Yuvraj-g1 <ys5299@srmist.edu.in>
2026-02-26 16:00:08 +0530

    UC2 - Hardcoded Palindrome Check implemented

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,7 +4,18 @@ public class PalindromeCheckerApp {
 
         System.out.println("Palindrome Checker App");
         System.out.println("Version 1.0");
-        System.out.println("Application Started");
 
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("It is Not a Palindrome");
+        }
     }
 }
